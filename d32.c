9ca5a9e83849bf6f478e99d3b1b3a426d7805da8
Anushka5678661 <negianushka332@gmail.com>
2026-03-04 18:12:17 +0530

    Day 32 of 100 day challenge

---
+++ d32.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int stack[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &stack[i]);
+    }
+
+    int m;
+    scanf("%d", &m);
+
+    int top = n - 1;
+    for (int i = 0; i < m; i++) {
+        if (top >= 0) {
+            top--;
+        }
+    }
+
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+
+    return 0;
+}
