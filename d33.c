76845bd68f39000801be68f63688e3495f27122c
Anushka5678661 <negianushka332@gmail.com>
2026-03-05 23:43:18 +0530

    Day 33 of 100 day challenge

---
+++ d33.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+int precedence(char op) {
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, k = 0;
+    
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+        char c = infix[i];
+        
+        if (isalnum(c)) {
+            postfix[k++] = c;
+        } else if (c == '(') {
+            push(c);
+        } else if (c == ')') {
+            while (top != -1 && stack[top] != '(') {
+                postfix[k++] = pop();
+            }
+            pop();
+        } else {
+            while (top != -1 && precedence(stack[top]) >= precedence(c)) {
+                postfix[k++] = pop();
+            }
+            push(c);
+        }
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+    printf("%s\n", postfix);
+
+    return 0;
+}
